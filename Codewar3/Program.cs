d78d8b13d3164765bf29ecc5613d9a6c0da8d0f5
Elijah Taylor <ElijahTaylor77@outlook.com>
2020-06-07 20:42:02 -0700

    new code

---
+++ Codewar3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Codewar3
+{
+    class Program
+    {
+        public static int[] ReverseSeq(int n)
+        {
+            int[] reversed = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                reversed[i] = n - i;
+            }
+            return reversed;
+        }
+        public static int[] ReverseSeq2(int n)
+        {
+            var list = new List<int>();
+            for (int i = n; i > 0; i--)
+                list.Add(i);
+            return list.ToArray();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
