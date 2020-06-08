e7cfc78aadd8740758af2f4b55d4414705292d33
Elijah Taylor <ElijahTaylor77@outlook.com>
2020-06-07 20:31:58 -0700

    new code

--- Codewar/Program.cs
+++ Codewar/Program.cs
@@ -9,6 +9,22 @@ namespace Codewar
         {
             return YourPoints > ClassPoints.Average();
         }
+        public static bool BetterThanAverage2(int[] ClassPoints, int YourPoints)
+        {
+            // calculate average points
+            int sum = 0;
+            foreach (var point in ClassPoints)
+            {
+                sum += point;
+            }
+
+            int avgPoint = Convert.ToInt32(sum / ClassPoints.Length);
+
+            // compare
+
+            return YourPoints > avgPoint ? true : false;
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
