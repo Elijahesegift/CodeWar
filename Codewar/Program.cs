a8e59297f69a5f2173832a3eff9cce9a0e8b7749
Elijah Taylor <ElijahTaylor77@outlook.com>
2020-06-07 20:21:41 -0700

    Add project files.

---
+++ Codewar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Codewar
+{
+    class Program
+    {
+        public static bool BetterThanAverage(int[] ClassPoints, int YourPoints)
+        {
+            return YourPoints > ClassPoints.Average();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
