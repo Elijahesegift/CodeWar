d880abc55faa700e6ea086f0dc03ac2711294198
Elijah Taylor <ElijahTaylor77@outlook.com>
2020-06-10 23:43:58 -0700

    new code

---
+++ Codewar4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewar4
+{
+    class Program
+    {
+        public static long[] Digitize(long n)
+        {
+            return n.ToString()
+                    .Reverse()
+                    .Select(t => Convert.ToInt64(t.ToString()))
+                    .ToArray();
+        }
+        public static long[] Digitize2(long n)
+        {
+            return n.ToString().Reverse().Select(x => long.Parse(x.ToString())).ToArray();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
