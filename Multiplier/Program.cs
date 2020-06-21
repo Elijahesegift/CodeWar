57bbe6cc537310cd0b1a4e9df66b17d0e50acb07
Elijah Taylor <ElijahTaylor77@outlook.com>
2020-06-21 16:02:15 -0700

    New Code

---
+++ Multiplier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
